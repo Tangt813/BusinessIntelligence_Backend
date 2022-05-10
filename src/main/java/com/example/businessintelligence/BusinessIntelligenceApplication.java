411f1131e5ee27812a36e4ab742d2fe8d0235299
tangt813201 <78403006+Tangt813@users.noreply.github.com>
2022-05-10 23:16:02 +0800

    init

---
+++ src/main/java/com/example/businessintelligence/BusinessIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.businessintelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessIntelligenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessIntelligenceApplication.class, args);
+    }
+
+}
